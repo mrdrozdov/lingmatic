ec8d197b1092cae3b5506c7013511d12b3d88a24
Andrew Drozdov <andrew@mrdrozdov.com>
2018-10-16 17:20:06 -0400

    Add postprocess heuristic.

--- lingmatic/engine/parse_comparison.py
+++ lingmatic/engine/parse_comparison.py
@@ -1,5 +1,7 @@
 import os
 
+punctuation_words = set(['.', ',', ':', '-LRB-', '-RRB-', '\'\'', '``', '--', ';', '-', '?', '!', '...', '-LCB-', '-RCB-'])
+
 
 def average_depth(parse):
     depths = []
@@ -53,13 +55,80 @@ class CompareF1(object):
         return '{} {:.3f}'.format(self.name, self.score)
 
 
+def get_spans(tree):
+    def helper(tr, idx=0):
+        if isinstance(tr, (str, int)):
+            return 1, []
+        left, left_spans = helper(tr[0], idx=idx)
+        right, right_spans = helper(tr[1], idx=idx+left)
+        span = [(idx, idx + left + right)]
+        spans = span + left_spans + right_spans
+        return left + right, spans
+
+    _, spans = helper(tree)
+
+    return spans
+
+
+def should_remove(x):
+    if not isinstance(x, str):
+        return False
+    if x in punctuation_words:
+        return True
+    return False
+
+
+def remove_punctuation(tr, last_only=False):
+    def helper(tree, last_only=False):
+        removed = None
+        if isinstance(tree, str):
+            return tree, removed
+
+        if last_only:
+            left = tree[0]
+        else:
+            left, _ = helper(tree[0], last_only=False)
+        right, removed = helper(tree[1], last_only=last_only)
+
+        if last_only:
+            left_skip = False
+        else:
+            left_skip = left is None or should_remove(left)
+        right_skip = right is None or should_remove(right)
+
+        if last_only and right_skip:
+            removed = right
+
+        if left_skip and right_skip:
+            return None, removed
+        elif left_skip:
+            return right, removed
+        elif right_skip:
+            return left, removed
+        return (left, right), removed
+
+    result, removed = helper(tr, last_only=last_only)
+
+    return result, removed
+
+
+def heuristic(pt):
+    tr, removed = remove_punctuation(pt.binary_parse_tree, last_only=True)
+    if removed is None:
+        return pt.binary_parse_spans, pt.binary_parse_tree
+    tr = (tr, removed)
+    spans = set(get_spans(tr))
+    return spans, tr
+
+
 class ParseComparison(object):
-    def __init__(self, comparisons=[CompareF1(), AverageDepth()], count_missing=False):
+    def __init__(self, comparisons=[CompareF1(), AverageDepth()], count_missing=False, postprocess=False):
         self.stats = {}
         self.stats['count'] = 0
         self.stats['skipped'] = 0
         self.comparisons = comparisons
         self.count_missing = count_missing
+        self.postprocess = postprocess
 
     def should_run(self, corpus_gt, corpus_pred, key):
         gt, pred, skip = None, None, False
@@ -73,11 +142,18 @@ class ParseComparison(object):
 
     def preprocess(self, gt, pred):
         skip = False
+
+        if self.postprocess:
+            pred.binary_parse_spans, pred.binary_parse_tree = heuristic(pred)
+
         return gt, pred, skip
 
     def run(self, corpus_gt, corpus_pred):
         seen = set()
         for key in tqdm(corpus_pred.keys()):
+
+            seen.add(key)
+
             gt, pred, skip = self.should_run(corpus_gt, corpus_pred, key)
 
             if skip:
@@ -91,7 +167,6 @@ class ParseComparison(object):
             for judge in self.comparisons:
                 judge.compare(gt, pred)
 
-            seen.add(key)
             self.stats['count'] += 1
 
         if self.count_missing:
@@ -121,6 +196,14 @@ if __name__ == '__main__':
     from tqdm import tqdm
 
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--limit', default=None, type=int)
+    parser.add_argument('--gt', default=os.path.expanduser('~/Downloads/ptb.jsonl'), type=str)
+    parser.add_argument('--pred', default=os.path.expanduser('~/Downloads/PRPN_parses/PRPNLM_ALLNLI/parsed_WSJ_PRPNLM_AllLI_ESLM.jsonl'), type=str)
+    parser.add_argument('--postprocess', action='store_true')
+    options = parser.parse_args()
+
+
     class DeserializeGT(ParseTreeDeserializeGroundTruth):
         def get_parse(self):
             return None
@@ -137,17 +220,14 @@ if __name__ == '__main__':
             return None
 
         def get_binary_parse_tree(self):
-            return None
+            if options.postprocess:
+                return super(DeserializePred, self).get_binary_parse_tree()
+            else:
+                return None
 
         def get_tokens(self):
             return None
 
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--limit', default=None, type=int)
-    parser.add_argument('--gt', default=os.path.expanduser('~/Downloads/ptb.jsonl'), type=str)
-    parser.add_argument('--pred', default=os.path.expanduser('~/Downloads/PRPN_parses/PRPNLM_ALLNLI/parsed_WSJ_PRPNLM_AllLI_ESLM.jsonl'), type=str)
-    options = parser.parse_args()
 
     limit = options.limit
 
@@ -162,6 +242,6 @@ if __name__ == '__main__':
     corpus_pred = {x.example_id: x for x in results}
 
     # Corpus Stats
-    ParseComparison(count_missing=True).run(corpus_gt, corpus_pred)
+    ParseComparison(count_missing=True, postprocess=options.postprocess).run(corpus_gt, corpus_pred)
     print('Count (Ground Truth): {}'.format(len(corpus_gt)))
     print('Count (Predictions): {}'.format(len(corpus_pred)))
